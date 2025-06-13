66a2bce18df1e98e61231e2c411026dc058a5d19
MewKay <miorakanto25z@gmail.com>
2025-06-14 01:20:57 +0300

    Create user router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const routes = require("./routes");
 app.use("/api/posts", routes.post);
 app.use("/api/authors", routes.author);
+app.use("/api/users", routes.user);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on PORT : ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const post = require("./post.router");
 const author = require("./author.router");
+const user = require("./user.router");
 
-module.exports = { post, author };
+module.exports = { post, author, user };
