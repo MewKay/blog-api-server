60f7e0b978081600a2b36eb899f013d9338b92e8
MewKay <miorakanto25z@gmail.com>
2025-06-18 11:24:04 +0300

    Add constants for validation ranges

---
+++ constants/validation.js
@@ -0,0 +1,12 @@
+const ranges = {
+  username: {
+    min: 4,
+    max: 15,
+  },
+  password: {
+    min: 8,
+    max: 255,
+  },
+};
+
+module.exports = { ranges };
--- middlewares/validation/validators/login.validator.js
+++ middlewares/validation/validators/login.validator.js
@@ -1,19 +1,22 @@
 const { ExpressValidator } = require("express-validator");
+const { ranges } = require("../../../constants/validation");
 
 const { body } = new ExpressValidator();
 
 const logInValidator = [
   body("username")
     .trim()
-    .isLength({ min: 4, max: 15 })
+    .isLength(ranges.username)
     .withMessage(
-      `Username is required to be between ${4} and ${15} characters.`,
+      `Username is required to be between ${ranges.username.min} and ${ranges.username.max} characters.`,
     )
     .isAlphanumeric()
     .withMessage("Username can only contain letters and numbers."),
   body("password")
-    .isLength({ min: 8, max: 255 })
-    .withMessage(`Password must be between ${8} and ${255} characters.`),
+    .isLength(ranges.password)
+    .withMessage(
+      `Password must be between ${ranges.password.min} and ${ranges.password.max} characters.`,
+    ),
 ];
 
 module.exports = logInValidator;
