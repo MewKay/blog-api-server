f94930c49a63b717f1bb2031ebf530b509824f38
MewKay <miorakanto25z@gmail.com>
2025-06-23 21:02:56 +0300

    Create error classes

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const Validation = require("./Validation.error");
+const Unauthorized = require("./Unauthorized.error");
+const NotFound = require("./NotFound.error");
+const Forbidden = require("./Forbidden.error");
+
+module.exports = {
+  Validation,
+  Unauthorized,
+  NotFound,
+  Forbidden,
+};
