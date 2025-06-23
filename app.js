27d14e531c9f0051b547e160978e00b616ce1132
MewKay <miorakanto25z@gmail.com>
2025-06-23 22:56:52 +0300

    Add general error handler middleware to app

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ require("./config/passport");
 
 const routes = require("./routes");
 const notFoundRoute = require("./middlewares/not-found-route");
+const errorHandler = require("./middlewares/error-handler");
 app.use("/api", routes.auth);
 app.use("/api/posts", routes.post);
 app.use("/api/authors", routes.author);
 
 app.use(notFoundRoute);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on PORT : ${PORT}`));
